49c074ef8fd199a1bce7171f7ed3d600ca620da6
ewertonilima <ewertoninaciolima@gmail.com>
2025-11-30 15:22:59 -0300

    init: 🎉 Starting project

---
+++ src/test/java/com/ewertonilima/jwtvalidator/JwtvalidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ewertonilima.jwtvalidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtvalidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
